48192eae525f64f319db5d8107e5bc9d2893cdbf
rayvivek881 <rayvivek881@gmail.com>
2022-08-19 08:22:54 +0530

    first Encrypt.js

--- constant/encrypt.js
+++ constant/encrypt.js
@@ -1,16 +1,15 @@
 
-const encrypt = (str) => {
+const Encrypt = (str) => {
     let x = str.length;
     let a = 0, p = 51, MOD = 1e9 + 9;
     let c, prev = 1;
     for (let i = 0; i < x; i++) {
         a += (str[i].charCodeAt(0)) * prev * p;
         a %= MOD;
-        prev *= 51;
-        prev %= MOD;
+        prev = (prev * 51) % MOD;
     }
     return a;
 }
 
 
-module.exports = encrypt;
+module.exports = Encrypt;
